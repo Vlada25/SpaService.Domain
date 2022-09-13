cd370cbde86cc3c927270a1406a9a87e289ae30c
Vlada <aladka03@gmail.com>
2022-09-13 14:27:32 +0300

    created models for user and service

---
+++ Messages/User/UserCreated.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaService.Domain.Messages.User
+{
+    public class UserCreated
+    {
+        public Guid Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+    }
+}
