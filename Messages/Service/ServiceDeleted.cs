915ec72ae9eac5a8ab09173469e171143a15d47f
Vlada <aladka03@gmail.com>
2022-09-13 14:45:51 +0300

    added some more classes

---
+++ Messages/Service/ServiceDeleted.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaService.Domain.Messages.Service
+{
+    public class ServiceDeleted
+    {
+        public Guid Id { get; set; }
+    }
+}
--- Messages/User/UserClientDeleted.cs
+++ Messages/User/UserClientDeleted.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace SpaService.Domain.Messages.User
 {
-    public class UserCreated
+    public class UserClientDeleted
     {
         public Guid Id { get; set; }
-        public string Surname { get; set; }
-        public string Name { get; set; }
-        public string MiddleName { get; set; }
     }
 }
